66feb463e532053d9199871bfecf08d17a628533
Alexvolkihar <Alexvolkihar@users.noreply.github.com>
2026-06-11 16:03:47 +0200

    security: obfuscate uniqueness error messages

--- src/Entity/RegistrationRequest.php
+++ src/Entity/RegistrationRequest.php
@@ -39,7 +39,7 @@ class RegistrationRequest
     #[ORM\Column(type: Types::STRING, length: 255, unique: true)]
     #[Assert\NotBlank(message: 'Le numéro de sécurité sociale est obligatoire.')]
     #[Assert\Regex(pattern: '/^[0-9]{15}$/', message: 'Le numéro de sécurité sociale doit être composé de 15 chiffres.')]
-    #[UniqueRegistrationField(field: 'socialSecurityNumber', message: 'Ce numéro de sécurité sociale est déjà utilisé.')]
+    #[UniqueRegistrationField(field: 'socialSecurityNumber', message: "Une demande d'inscription existe déjà pour les informations fournies.")]
     private ?string $socialSecurityNumber = null;
 
     #[ORM\Column(type: Types::STRING, length: 255)]
@@ -49,7 +49,7 @@ class RegistrationRequest
     #[ORM\Column(type: Types::STRING, length: 255, unique: true)]
     #[Assert\NotBlank(message: 'Le numéro d\'accréditation CERFA 666 est obligatoire.')]
     #[Assert\Regex(pattern: '/^CERFA-666-[a-zA-Z0-9]+$/', message: 'Le numéro d\'accréditation doit respecter le format CERFA-666-XXX.')]
-    #[UniqueRegistrationField(field: 'fighterCertificationNumber', message: 'Ce numéro d\'accréditation est déjà utilisé.')]
+    #[UniqueRegistrationField(field: 'fighterCertificationNumber', message: "Une demande d'inscription existe déjà pour les informations fournies.")]
     private ?string $fighterCertificationNumber = null;
 
     #[ORM\Column(type: Types::STRING, length: 255)]
